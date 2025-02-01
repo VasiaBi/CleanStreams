d38350454926aef520c0a3f190e80c40b377ce58
VasiaBi <vasiabilali@gmail.com>
2025-02-01 17:50:53 +0200

    file deletion

--- drone-functionality.py
+++ drone-functionality.py
@@ -25,11 +25,11 @@ def get_battery_level():
 def remaining_flight_time():
     drone = tello_sdk.Tello()
     #If percentage is returned, then "/100" is not needed
-    rem_flight_time = max_flight_time*get_battery_level/100
+    rem_flight_time = max_flight_time*get_battery_level()/100
     return rem_flight_time
 
 # Route points for screen 2c
-def get_starting_point():
+def get_starting_point(flight_id):
     drone = tello_sdk.Tello()
 
     # Connect to the database. Need to replace database name
@@ -40,13 +40,15 @@ def get_starting_point():
     # Assuming the table has two columns, ID and route_points
     # Replace table name (flights) and column name (route_points).
     # ID: Can we use the flight name combined with the flight time sto that it is unique?
-    cursor.execute("SELECT route_points FROM flights WHERE id = ?", (id,))
+    cursor.execute("SELECT route_points FROM flights WHERE id = ?", (flight_id,))
     result = cursor.fetchone()
 
     # A result of this type is expected: (ID, '{"route_points": [coord1, coord2, coord3, etc]}')
     if result:
-        route_points = json.loads(result[1])
+        route_points = json.loads(result[0])
         return route_points
+    else:
+        return None
     
     # Show route point on the map
 
@@ -57,4 +59,4 @@ def get_starting_point():
     # Close the connection
     conn.close()
 
-    # Next point
+    
